ab0a0e46632449dc67bfddba58aa7fd9fac00c21
Hannan84 <hannansarkar84@gmail.com>
2023-12-10 00:46:40 +0600

    create change password

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Hash;
+use App\Models\User;
 class AdminController extends Controller
 {
     public function __construct()
@@ -34,16 +35,23 @@ class AdminController extends Controller
     // password update
     public function passwordUpdate(Request $request){
 
-        // $validated = $request->validate([
-        //     'oldpassword' => 'required',
-        //     'password' => 'required|min:6|confirmed',
-        // ]);
+        $validated = $request->validate([
+            'oldpassword' => 'required',
+            'password' => 'required|min:6|same:passwordConfirmation',
+        ]);
 
         $old_pass = Auth::user()->password;
-        if(Hash::check($old_pass,$request->oldpassword)){
-
+        if(Hash::check($request->oldpassword,$old_pass)){
+            $user = User::find(Auth::user()->id);
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+            Auth::logout();
+            toastr()->success('Password change success!'); 
+            return redirect()->route('login');
         }else{
-            
+            toastr()->error('OldPassword not match!'); 
+            return redirect()->back();
         }
     }
 }

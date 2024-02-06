72af2d38f85f5140e6b1d10a3ada26f328764941
Hannan84 <hannansarkar84@gmail.com>
2024-02-06 23:46:33 +0600

    update

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Coupon;
-use DataTables;
+use Yajra\DataTables\Facades\DataTables;
 
 class CouponController extends Controller
 {

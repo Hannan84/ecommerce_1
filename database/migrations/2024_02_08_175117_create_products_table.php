a9ed1497d85addb96893180e47775db0a9ccde83
Hannan84 <hannansarkar84@gmail.com>
2024-02-09 12:10:55 +0600

    create product scema

--- database/migrations/2024_02_08_175117_create_products_table.php
+++ database/migrations/2024_02_08_175117_create_products_table.php
@@ -15,6 +15,30 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained('categories', 'id')->onDelete('cascade');
+            $table->foreignId('subcategory_id')->constrained('subcategories', 'id')->onDelete('cascade');
+            $table->integer('childcategory_id')->nullable();
+            $table->integer('brand_id')->nullable();
+            $table->integer('warehouse_id')->nullable();
+            $table->integer('flash_deal_id')->nullable();
+            $table->integer('pickuppoint_id')->nullable();
+            $table->integer('admin_id')->nullable();
+            $table->string('name');
+            $table->string('code');
+            $table->string('unit')->nullable();
+            $table->string('tags')->nullable();
+            $table->string('video')->nullable();
+            $table->string('pur_price')->nullable();
+            $table->string('sell_price')->nullable();
+            $table->string('dis_price')->nullable();
+            $table->string('stock_qty')->nullable();
+            $table->string('description')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->string('images')->nullable();
+            $table->integer('today_deal')->nullable();
+            $table->integer('cash_od')->nullable();
+            $table->enum('featured',['yes','no'])->default('no');
+            $table->enum('status',['active','deactive'])->default('deactive');
             $table->timestamps();
         });
     }

7a17bb3902f91c5e1bc27ec9cdc5f8e2ce35dd31
Hannan84 <hannansarkar84@gmail.com>
2024-02-15 07:33:32 +0600

    product create

--- resources/views/layouts/admin_partial/sidebar.blade.php
+++ resources/views/layouts/admin_partial/sidebar.blade.php
@@ -86,6 +86,29 @@
                         <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-copy"></i>
                             <p>
+                                Products
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('coupon.index') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Create</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Product</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-copy"></i>
+                            <p>
                                 Offers
                                 <i class="fas fa-angle-left right"></i>
                             </p>
